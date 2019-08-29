facd2e58e2a9e7d815da5c495caf5489ae1edacd
Tommaso Garuglieri <tommaso.garuglieri@travelappeal.com>
2019-08-29 11:07:32 +0200

    Added support for fifo queues

--- aws/publisher.go
+++ aws/publisher.go
@@ -2,16 +2,20 @@ package aws
 
 import (
 	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/sqs"
+	"github.com/satori/go.uuid"
 )
 
 type Publisher struct {
 	Sqs *sqs.SQS
+	id  string
 }
 
 func NewPublisher(SQS *sqs.SQS) *Publisher {
 	return &Publisher{
 		Sqs: SQS,
+		id:  uuid.NewV4().String(),
 	}
 }
 
@@ -36,8 +40,9 @@ func (p *Publisher) Redeliver(messages []*sqs.Message, queue string) error {
 	batch := make([]*sqs.SendMessageBatchRequestEntry, len(messages))
 	for i := 0; i < len(messages); i++ {
 		batch[i] = &sqs.SendMessageBatchRequestEntry{
-			Id:          messages[i].MessageId,
-			MessageBody: messages[i].Body,
+			Id:             messages[i].MessageId,
+			MessageBody:    messages[i].Body,
+			MessageGroupId: aws.String(p.id),
 		}
 	}
 
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.12
 
 require (
 	github.com/aws/aws-sdk-go v1.20.6
+	github.com/satori/go.uuid v1.2.0
 	github.com/spf13/cobra v0.0.5
 )
